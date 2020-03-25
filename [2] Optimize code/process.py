cc0ac58cf87b3f987d0c8daf9d91f0a2d8948b18
volinhbao <linhbaovo@gmail.com>
2020-03-25 10:16:38 +0700

    add timing

--- [2] Optimize code/process.py
+++ [2] Optimize code/process.py
@@ -1,8 +1,11 @@
 import os
 import argparse
+import time
+
+START_TIME = time.time()
 
 
-def load_dict(path_dict:str):
+def load_dict(path_dict: str):
     """
     @arguments:
         - path_dict (string): the path of dictionary/syllabus file.
@@ -10,11 +13,12 @@ def load_dict(path_dict:str):
         - A list of word in dictionary file 
     """
 
-    with open(path_dict, 'r') as f:
+    with open(path_dict, 'r', encoding="utf8") as f:
         content = f.read()
         word_list = content.splitlines()
         return word_list
     return []
+
 
 def load_test(path_test: str):
     """
@@ -23,7 +27,7 @@ def load_test(path_test: str):
     @return values:
         - the dictionary contain all word in test document and the frequency of them. 
     """
-    with open(path_test, 'r') as f:
+    with open(path_test, 'r', encoding="utf8") as f:
         content = f.read()
         word_list = list(map(str, content.split(" ")))
 
@@ -38,13 +42,15 @@ def load_test(path_test: str):
 
     return None
 
+
 def process(dictionary: list, document: dict):
     """
     @parameter:
         dictionary:
         documents:
     @return values:
-        - A dictionary that the key value is the word in the dictionary and the value is the frequency of word appear in the documents.
+        - A dictionary that the key value is the word in the dictionary and the value is the frequency of word appear
+        in the documents.
 
     """
     # Solve the question a: count the number of word in the dictionary exist in documents.
@@ -52,11 +58,12 @@ def process(dictionary: list, document: dict):
     for word in dictionary:
         if word in document:
             count_appear += 1
-    
+
     # Solve the question b: calculate the frequency.
     frequency = count_appear / len(dictionary) * 100
 
     return count_appear, frequency
+
 
 def main(args):
     path_dictionary = args.dict_path
@@ -73,10 +80,12 @@ def main(args):
     count_appear, frequency = process(dictionary=dictionary, document=document)
     print(f"The number of word in dictionary appear in the test document is {count_appear}")
     print(f"The percentage is {frequency}%")
+    print("--- %s seconds ---" % (time.time() - START_TIME))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Count the frequency of the words in dictionary appear in test file.')
-    parser.add_argument("--dict_path",  help="The path of dictionary", default="syllables.txt")
+    parser.add_argument("--dict_path", help="The path of dictionary", default="syllables.txt")
     parser.add_argument("--test_path", help="The path of test file.", default="test.txt")
     args = parser.parse_args()
 
